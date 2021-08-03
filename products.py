3b851f654fcbb9b3624152d494a0854b25ab0f73
Nicole <67175550+Nicole1707@users.noreply.github.com>
2021-08-03 11:51:16 -0400

    Add files via upload

---
+++ products.py
@@ -0,0 +1,7 @@
+products=[
+    {"name": "Arroz", "price": 35, "quantity": 5 },
+    {"name": "Maiz", "price": 25, "quantity": 4 },
+    {"name": "Lentejas", "price": 35, "quantity": 6 },
+    {"name": "Habichuelas", "price": 75, "quantity": 5 },
+    {"name": "Gandueles", "price": 50, "quantity": 5 }
+]
